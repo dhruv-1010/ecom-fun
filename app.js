5533d00002584b74ac5b6c00a59a16f6c173be4f
Dhruv <dhruv.singh_cs21@gla.ac.in>
2023-07-21 15:56:51 +0530

    rest api build

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path=require('path');
-
+const methodover = require('method-override')
 const port = process.env.PORT || 8000;
 const mongoose = require('mongoose');
 const seedDB = require('./seed');
 const productRoutes = require('./routes/productRoutes');
+
+
+
+
 // setting path
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views'))
 app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.urlencoded({extended:true}));//body parser
+app.use(methodover('_method'))
 // 
+
 // 
 app.use(productRoutes);
+
 mongoose.connect(process.env.DB_URL).
 then(()=>{
     console.log('db conneted');})
@@ -23,18 +31,7 @@ then(()=>{
 
 // db connection 
 // seedDB();
-
-
-
-
-
-
-
-
-
-
-
-
+// now we need make an view to add data to db 
 
 app.listen(port,()=>{
     console.log(`connected at port ${port}`)
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,15 +3,57 @@ const Product = require('../models/Product');
 
 // express router acts like mini server
 const router = express.Router();
+
+
 // display all the product
 router.get('/products',async (req,res)=>{
     // finding all the products
     let products =  await Product.find({});
     res.render('products/index',{products})
 })
+// second route show the form to add a new prodcut error
+router.get('/products/new',async (req,res)=>{
+    res.render('products/new');
+})
+// third route post request
+router.post('/products',async (req,res)=>{
+    let {name ,img ,price ,desc}=  req.body;
+    await Product.create({name,img,price,desc});
+    res.redirect('/products');
+})
+// show a single product
+router.get('/products/:id',async (req,res)=>{
+    let {id}=req.params;
+    let found = await Product.findById(id);
+    // console.log(found);
+    res.render('products/show',{found})
+})
+router.get('/products/:id/edit',async (req,res)=>{
+    let {id} = req.params;
+    let found = await Product.findById(id);
+    // console.log("mika");
+    res.render('products/edit',{found});
+})
 
 
+router.patch('/products/:id',async (req,res)=>{
+    let { id }= req.params;
+    let {name ,img ,price ,desc}=  req.body;
+    await Product.findByIdAndUpdate(id,{name,desc,price,img});
+    res.redirect('/products');
+})
+/*
 
+/*
+// route for editing
+*/
+
+// route for deleteing shit
+router.delete('/products/:id',async (req,res)=>{
+    let {id} = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
 
 
 
